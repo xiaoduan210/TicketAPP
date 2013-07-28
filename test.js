d32e976829cdad6e99de9ce52112974bb3502f8a
xiaoduan210 <xiaoduan210@qq.com>
2013-07-28 19:36:56 +0800

    ttt
    tttt

---
+++ test.js
@@ -0,0 +1,75 @@
+{
+  "subject": "关键词上线日期确认后收余款，麻烦帮忙弄一下，谢谢！！！",
+  "companyName": "瑞安市经纬气动液压元件成套有限公司",
+  "fromUserName": "肖小丽",
+  "assigneeUserName": "胡凯.",
+  "joinUserName": "居丽芳,",
+  "status": "已确认",
+  "hopeDoneTime": "2013-07-12 20:12:05",
+  "creatTime": "2013-07-12 18:12:11",
+  "Posts": [
+    {
+      "id": "5df65cb4-a2fd-46d7-a2df-52f1b81f18c7",
+      "creatTime": "2013-07-12 18:12:11",
+      "author": "肖小丽",
+      "headImageUrl": "http://a2.twimg.com/sticky/default_profile_images/default_profile_1_normal.png",
+      "message": "<P>麻烦把黄召去年签订的关键词，具体的上线日期发给我一下，客户要让我<IMG src=\"http://edit.wzlianxin.com/net/upload/胡凯/2013-07-12/093961c7-0aab-4872-8870-f1c70bef7a8e.jpg\">理好之后然后到时双方确定个时间，才算是续费日期，这个弄好后，他说才给余款的，只要弄好这个就给余款，这周跟其他两个团队PK，所以麻烦早点提供给我，我好早点去客户那里收款，谢谢！！！</P>\r\n<P></P>\r\n<P>还有去年黄召签的那份合同，客户说有应该要有他们签字盖章的，现在系统里的是没有盖章和签字的，能帮忙找下还有其他的合同吗？或者说有客户签字盖章的合同<IMG src=\"http://edit.wzlianxin.com/net/upload/胡凯/2013-07-12/093961c7-0aab-4872-8870-f1c70bef7a8e.jpg\">没有上传？</P>"
+    },
+    {
+      "id": "d1526c99-18ae-47c7-b718-41725e12fdb2",
+      "creatTime": "2013-07-12 19:47:36",
+      "author": "胡凯",
+      "headImageUrl": "http://a2.twimg.com/sticky/default_profile_images/default_profile_1_normal.png",
+      "message": "<P>签字盖章的东西我这里没有。</P>"
+    },
+    {
+      "id": "53f86ad9-a1ac-4739-a0dd-892bbec21d74",
+      "creatTime": "2013-07-12 20:08:18",
+      "author": "肖小丽",
+      "headImageUrl": "http://a2.twimg.com/sticky/default_profile_images/default_profile_1_normal.png",
+      "message": "<P>要问赵总吗？</P>"
+    },
+    {
+      "id": "2f5c3e45-66d1-43ae-923b-da3f94ff5c24",
+      "creatTime": "2013-07-12 21:09:43",
+      "author": "胡凯",
+      "headImageUrl": "http://a2.twimg.com/sticky/default_profile_images/default_profile_1_normal.png",
+      "message": "<P>精密调压阀2012.8.22<BR>真空发生器2012.8.26 &nbsp; <BR>回转气缸2012.10.6 <BR>旋转气缸2012.10.13 <BR>SMC精密减压阀2012.6.7 <BR>防爆阀 2013.5.17<BR>气动马达2013.5.25<BR>气动冲床2012.6.7<BR>气动旋转接头2012.6.7 </P>"
+    },
+    {
+      "id": "7d7811fe-918f-4c64-839b-4ff5f12fcf15",
+      "creatTime": "2013-07-12 21:10:15",
+      "author": "胡凯",
+      "headImageUrl": "http://a2.twimg.com/sticky/default_profile_images/default_profile_1_normal.png",
+      "message": "<P><IMG src=\"http://edit.wzlianxin.com/net/upload/胡凯/2013-07-12/093961c7-0aab-4872-8870-f1c70bef7a8e.jpg\">这个是当时和黄召的聊天记录，</P>"
+    },
+    {
+      "id": "0f1f7f7f-8f16-43cb-a46e-c50f01d4e45b",
+      "creatTime": "2013-07-12 21:10:18",
+      "author": "胡凯",
+      "headImageUrl": "http://a2.twimg.com/sticky/default_profile_images/default_profile_1_normal.png",
+      "message": "<P><SPAN style=\"COLOR: green\">正在进行中!</SPAN></P>\r\n<P></P>"
+    },
+    {
+      "id": "fce70856-04fa-4e98-bb22-57b4870b8d1d",
+      "creatTime": "2013-07-12 21:10:20",
+      "author": "胡凯",
+      "headImageUrl": "http://a2.twimg.com/sticky/default_profile_images/default_profile_1_normal.png",
+      "message": "<P><SPAN style=\"COLOR: green\">已解决!</SPAN></P>\r\n<P></P>"
+    },
+    {
+      "id": "6427c57d-ce5f-4337-8565-ee3ca78faf7c",
+      "creatTime": "2013-07-12 21:12:07",
+      "author": "胡凯",
+      "headImageUrl": "http://a2.twimg.com/sticky/default_profile_images/default_profile_1_normal.png",
+      "message": "<P><IMG src=\"http://edit.wzlianxin.com/net/upload/胡凯/2013-07-12/ace3b16a-2166-4627-9c5a-e8d4d1e479e6.jpg\"> 这个是2012年10月6日的时候一个分站的结果</P>"
+    },
+    {
+      "id": "a5e14cb7-bebf-4487-9c6f-de990eb1abc3",
+      "creatTime": "2013-07-12 21:18:14",
+      "author": "肖小丽",
+      "headImageUrl": "http://a2.twimg.com/sticky/default_profile_images/default_profile_1_normal.png",
+      "message": "<P><SPAN style=\"COLOR: green\"><BR>\r\n<DIV class=quote>\r\n<BLOCKQUOTE><SPAN style=\"FONT-SIZE: 12px\"><SPAN style=\"COLOR: #999999\">胡凯 发表于 07-12 21:10</SPAN> <A href=\"#fce70856-04fa-4e98-bb22-57b4870b8d1d\"><IMG src=\"http://att.wzlianxin.com/Images/back.gif\" border=0></A></SPAN><BR>\r\n<P><SPAN style=\"COLOR: green\">已解决!</SPAN></P>\r\n<P></P></BLOCKQUOTE></DIV>\r\n<P>确认已完成. </P></SPAN>\r\n<P></P>\r\n<P></P>"
+    }
+  ]
+}
